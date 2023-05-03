816e9436e4f1c5f8756e02b3825fff44f16949e5
James Gower <66065325+James-G72@users.noreply.github.com>
2023-05-04 00:13:05 +0100

    Changing calculation to a function that terminates early to save time

--- 2023/Round A - Colliding Encoding.py
+++ 2023/Round A - Colliding Encoding.py
@@ -1,3 +1,18 @@
+def actual_calc(string_list, map_dict):
+    """
+    A function that returns true for collisions and false for no collisions
+    :param string_list: input of all strings to compare in a list
+    :return: True or False
+    """
+    comp_list = set()
+    for string in string_list:
+        cipher = ''.join(str(map_dict[letter]) for letter in string)
+        if cipher in comp_list:
+            return True
+        comp_list.add(cipher)
+    return False
+
+
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 Cases = int(input())
 for case in range(0,Cases):
@@ -8,12 +23,9 @@ for case in range(0,Cases):
 
     encode = []
     collide = "NO"
-    for tests in range(0,int(input())):
-        check_set = ""
-        for letter in input():
-            check_set += str(mapping_dict[letter])
-        encode.append(check_set)
-    if len(encode) != len(set(encode)):
-        collide = "YES"
-
-    print('Case #'+str(case+1)+': '+collide)
+    options = int(input())
+    all_strings = [input().strip() for i in range(options)]
+    if actual_calc(all_strings, mapping_dict):
+        print('Case #'+str(case+1)+': YES')
+    else:
+        print('Case #'+str(case+1)+': NO')
